c04fb42cc3973697fa2f93d87b94b57a8d7361eb
Andrew Hughes <andrewcarterhughes@gmail.com>
2019-05-08 15:02:03 -0700

    fixed form-auth

--- form-auth/src/main/java/com/okta/springsecurityauth/SecurityConfiguration.java
+++ form-auth/src/main/java/com/okta/springsecurityauth/SecurityConfiguration.java
@@ -10,11 +10,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.antMatcher("/**")
+        http
             .authorizeRequests()
             .anyRequest().authenticated()
             .and()
-            .oauth2Login();
+            .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .withUser("user")
+            .password("{noop}pass") // Spring Security 5 requires specifying the password storage format
+            .roles("USER");
     }
     
 }
